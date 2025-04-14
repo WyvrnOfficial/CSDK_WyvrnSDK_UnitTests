a0f1699c04424663fd71d6f9204d5b4b019e3093
Tim Graupmann <tgraupmann@gmail.com>
2025-04-14 10:55:09 -0700

    Remove dependency on WyvrnSDK DLLs

--- WyvrnAPI.cpp
+++ WyvrnAPI.cpp
@@ -1,4 +1,5 @@
 #include "WyvrnAPI.h"
+#include "UnicodeWyvrnAPI.h"
 #include "WyvrnErrors.h"
 #include "WyvrnLogger.h"
 #include "VerifyLibrarySignature.h"
@@ -6,126 +7,123 @@
 #include <tchar.h>
 
 
-#ifdef _WIN64
-#define WYVRNSDK_LIBRARY_DLL	L"WyvrnSDK64.dll"
-#else
-#define WYVRNSDK_LIBRARY_DLL	L"WyvrnSDK.dll"
-#endif
-
-
-using namespace WyvrnSDK;
 using namespace std;
+using namespace WyvrnSDK::Implementation;
 
-HMODULE WyvrnAPI::_sLibrary = nullptr;
-bool WyvrnAPI::_sInvalidSignature = false;
-bool WyvrnAPI::_sIsInitializedAPI = false;
+namespace WyvrnSDK {
 
-#define WYVRNSDK_DECLARE_METHOD_IMPL(Signature, FieldName) Signature WyvrnAPI::FieldName = nullptr;
+	bool WyvrnAPI::_sIsInitializedAPI = false;
+	bool WyvrnAPI::_sInitialized = false;
 
-#pragma region API declare assignments
-WYVRNSDK_DECLARE_METHOD_IMPL(PLUGIN_CORE_INIT_SDK, CoreInitSDK);
-WYVRNSDK_DECLARE_METHOD_IMPL(PLUGIN_CORE_SET_EVENT_NAME, CoreSetEventName);
-WYVRNSDK_DECLARE_METHOD_IMPL(PLUGIN_CORE_UNINIT, CoreUnInit);
+	int WyvrnAPI::InitAPI()
+	{
+		if (_sIsInitializedAPI)
+		{
+			return RZRESULT_SUCCESS;
+		}
+
+		if (_sInitialized)
+		{
+			return RZRESULT_SUCCESS;
+		}
+
+		RZRESULT result = UnicodeWyvrnAPI::InitAPI();
+		if (result == RZRESULT_SUCCESS)
+		{
+			_sIsInitializedAPI = true;
+		}
+		return result;
+	}
+
+	bool WyvrnAPI::GetIsInitializedAPI()
+	{
+		if (!_sIsInitializedAPI)
+		{
+			return false;
+		}
+		return UnicodeWyvrnAPI::GetIsInitializedAPI();
+	}
+
+	int WyvrnAPI::UninitAPI()
+	{
+		if (!_sIsInitializedAPI)
+		{
+			return RZRESULT_SUCCESS;
+		}
+		RZRESULT result = UnicodeWyvrnAPI::UninitAPI();
+		if (result == RZRESULT_SUCCESS)
+		{
+			_sIsInitializedAPI = false;
+		}
+		return result;
+	}
+
+#pragma region API declare prototypes
+
+	/*
+		Direct access to low level API.
+	*/
+	RZRESULT WyvrnAPI::CoreInitSDK(WyvrnSDK::APPINFOTYPE* appInfo)
+	{
+		if (!_sIsInitializedAPI)
+		{
+			RZRESULT result = WyvrnAPI::InitAPI();
+			if (result != RZRESULT_SUCCESS)
+			{
+				return result;
+			}
+			if (!_sIsInitializedAPI)
+			{
+				return RZRESULT_FAILED;
+			}
+		}
+		if (_sInitialized)
+		{
+			return RZRESULT_SUCCESS;
+		}
+		RZRESULT result = UnicodeWyvrnAPI::CoreInitSDK(appInfo);
+		if (result == RZRESULT_SUCCESS)
+		{
+			_sInitialized = true;
+		}
+		return result;
+	}
+	/*
+		Direct access to low level API.
+	*/
+	RZRESULT WyvrnAPI::CoreSetEventName(LPCTSTR name)
+	{
+		if (!_sIsInitializedAPI)
+		{
+			return -1;
+		}
+		if (!_sInitialized)
+		{
+			return -1;
+		}
+		return UnicodeWyvrnAPI::CoreSetEventName(name);
+	}
+	/*
+		Direct access to low level API.
+	*/
+	RZRESULT WyvrnAPI::CoreUnInit()
+	{
+		if (!_sIsInitializedAPI)
+		{
+			return RZRESULT_SUCCESS;
+		}
+		if (!_sInitialized)
+		{
+			return RZRESULT_SUCCESS;
+		}
+		RZRESULT result = UnicodeWyvrnAPI::CoreUnInit();
+		if (result == RZRESULT_SUCCESS)
+		{
+			_sInitialized = false;
+		}
+		return result;
+
+	}
 #pragma endregion
 
-#define WYVRNSDK_VALIDATE_METHOD(Signature, FieldName) FieldName = (Signature) GetProcAddress(library, "Plugin" #FieldName); \
-if (FieldName == nullptr) \
-{ \
-	cerr << "Failed to find method: " << ("Plugin" #FieldName) << endl; \
-    return -1; \
-}
-
-int WyvrnAPI::InitAPI()
-{
-	// abort load if an invalid signature was detected
-	if (_sInvalidSignature)
-	{
-		return RZRESULT_DLL_INVALID_SIGNATURE;
-	}
-
-	if (_sIsInitializedAPI)
-	{
-		return 0;
-	}
-
-	wchar_t filename[MAX_PATH]; //this is a char buffer
-	GetModuleFileNameW(NULL, filename, sizeof(filename));
-
-	std::wstring path;
-	const size_t last_slash_idx = std::wstring(filename).rfind('\\');
-	if (std::string::npos != last_slash_idx)
-	{
-		path = std::wstring(filename).substr(0, last_slash_idx);
-	}
-
-	path += L"\\";
-	path += WYVRNSDK_LIBRARY_DLL;
-
-	// check the library file version
-	if (!VerifyLibrarySignature::IsFileVersionSameOrNewer(path.c_str(), 2, 0, 1, 6))
-	{
-		WyvrnLogger::fprintf(stderr, "Detected old version of WyvrnSDK Library!\r\n");
-		return RZRESULT_DLL_NOT_FOUND;
-	}
-
-#ifdef CHECK_WYVRNSDK_LIBRARY_SIGNATURE
-	// verify the library has a valid signature
-	_sInvalidSignature = !VerifyLibrarySignature::VerifyModule(path);
-#endif
-
-	if (_sInvalidSignature)
-	{
-		WyvrnLogger::fprintf(stderr, "WyvrnSDK Library has an invalid signature!\r\n");
-		return RZRESULT_DLL_INVALID_SIGNATURE;
-	}
-
-	HMODULE library = LoadLibrary(path.c_str());
-	if (library == NULL)
-	{ 
-		WyvrnLogger::fprintf(stderr, "Failed to load WyvrnSDK Library!\r\n");
-        return RZRESULT_DLL_NOT_FOUND;
-	}
-
-	_sLibrary = library;
-	
-	//WyvrnLogger::fprintf(stderr, "Loaded WyvrnSDK DLL!\r\n");
-
-#pragma region API validation
-WYVRNSDK_VALIDATE_METHOD(PLUGIN_CORE_INIT_SDK, CoreInitSDK);
-WYVRNSDK_VALIDATE_METHOD(PLUGIN_CORE_SET_EVENT_NAME, CoreSetEventName);
-WYVRNSDK_VALIDATE_METHOD(PLUGIN_CORE_UNINIT, CoreUnInit);
-#pragma endregion
-
-	//WyvrnLogger::printf(stdout, "Validated all DLL methods [success]\r\n");
-	_sIsInitializedAPI = true;
-	return 0;
-}
-
-bool WyvrnAPI::GetIsInitializedAPI()
-{
-	return _sIsInitializedAPI;
-}
-
-#undef WYVRNSDK_DECLARE_METHOD_CLEAR
-#define WYVRNSDK_DECLARE_METHOD_CLEAR(FieldName) WyvrnAPI::FieldName = nullptr;
-
-int WyvrnAPI::UninitAPI()
-{
-	if (nullptr != _sLibrary)
-	{
-		FreeLibrary(_sLibrary);
-		_sLibrary = nullptr;
-	}
-
-#pragma region Free API Methods
-
-	WYVRNSDK_DECLARE_METHOD_CLEAR(CoreInitSDK);
-	WYVRNSDK_DECLARE_METHOD_CLEAR(CoreSetEventName);
-	WYVRNSDK_DECLARE_METHOD_CLEAR(CoreUnInit);
-
-#pragma endregion
-
-	_sIsInitializedAPI = false;
-	
-	return 0;
 }
--- WyvrnSDKTypes.h
+++ WyvrnSDKTypes.h
@@ -1,25 +1,27 @@
-﻿ #ifndef _WYVRNSDKTYPES_H_
- #define _WYVRNSDKTYPES_H_
+﻿//! \file WyvrnSDKTypes.h
+//! \brief Data types.
 
+#ifndef _WYVRNSDKTYPES_H_
+#define _WYVRNSDKTYPES_H_
+
+#pragma once
+
+#define LEAN_AND_MEAN
 #include <Windows.h>
-#define WIN32_LEAN_AND_MEAN
-  
- #pragma once
-  
- typedef LONG            RZRESULT;
 
-  
- namespace WyvrnSDK
- {
-     typedef struct APPINFOTYPE
-     {
-         wchar_t Title[256];           
-         wchar_t Description[1024];
-         struct Author
-         {
-             wchar_t Name[256];
-             wchar_t Contact[256];
-         } Author;
+typedef LONG            RZRESULT;           //!< Return result.
+
+namespace WyvrnSDK
+{
+    typedef struct APPINFOTYPE
+    {
+        wchar_t Title[256];
+        wchar_t Description[1024];
+        struct Author
+        {
+            wchar_t Name[256];
+            wchar_t Contact[256];
+        } Author;
          //SupportedDevice = 
          //    0x01 | // Keyboards
          //    0x02 | // Mice
@@ -27,12 +29,12 @@
          //    0x08 | // Mousepads
          //    0x10 | // Keypads
          //    0x20   // ChromaLink devices
-         const DWORD SupportedDevice = 63;
+        const DWORD SupportedDevice = 63;
          //Category = 
          //    0x01 | // App
          //    0x02 | // Game
-         DWORD Category;             
-     } APPINFOTYPE;
- }
-  
- #endif
+        DWORD Category;
+    } APPINFOTYPE;
+}
+
+#endif
